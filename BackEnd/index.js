29945cf8fc69aa6402902e743907b1d3720e692f
Md sha alam bulbul <79450874+bulbulkhan420@users.noreply.github.com>
2024-04-25 00:45:39 +0600

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,7 +3,6 @@ const env=require('dotenv').config();
 const app=express();
 const cors=require("cors");
 const {route}=require('./route.js');
-let http=require('http');
 let PORT=3002;
 app.use(cors());
 app.use(express.json());
@@ -13,36 +12,7 @@ app.use(express.static('uploads'));
 app.get("/",(req,res)=>{
     res.send("bulbul");
 });
-let server=http.createServer(app);
-let {Server}=require('socket.io');
-const { datamessage } = require('./database.js');
-let io=new Server(server,{
-    cors:{
-        origin:"https://class-vercel-frontend.vercel.app",
-        methods:["GET","POST"]
-    }
-})
-let tr=0;
-io.on('connection',(socket)=>{
-   
-    socket.on("send",async (data)=>{
-        socket.join(data.year);
-        tr++;
-        socket.to(data.year).emit("update",tr);
-    let v=await datamessage.insertMany([{name:data.id,year:data.year,message:data.msg}]);
-    if(v){
-        let p=true;
-        socket.emit('check',p);
-    }
-    else{
-        let p=false;
-        socket.emit('check',p);
-    }
-    
 
-    })
-
-})
-server.listen(PORT,(er)=>{
+app.listen(PORT,(er)=>{
     console.log("sucess");
 });
